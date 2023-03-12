8cbbee815030c207f9c6ad1ac26366c1f021dc75
Pavlo Papanov <papan41k@gmail.com>
2023-03-12 12:29:30 +0200

    Refactoring homework #15

--- homework-15/js/common.js
+++ homework-15/js/common.js
@@ -6,48 +6,36 @@ function Tasks(_input, _wrapper) {
 	this.addItem = (event) => {
 		event.preventDefault();
 		this.input = event.target.querySelector(_input);
-		wrapper.insertAdjacentHTML('beforeend', this.createItem(this.input.value));
+		wrapper.appendChild(this.createItem(this.input.value));
 		this.input.value = '';
-		this.checkStatus();
-		this.deleteItem();
 	}
 
 	this.createItem = function (description) {
-		return `
-			<div class="todo-item">
-				<input type="checkbox" class="js-todo-item__checkbox">
+		const newItem = document.createElement('div');
+		newItem.classList.add('todo-item');
+		newItem.innerHTML = (
+			`<input type="checkbox" class="js-todo-item__checkbox">
 				<div class="todo-item__description">${description}</div>
-				<button class="todo-item__delete js-todo-item__delete">Удалить</button>
-			</div>
-		`;
+				<button class="todo-item__delete js-todo-item__delete">Удалить</button>`
+		);
+		newItem.querySelector('.js-todo-item__checkbox').addEventListener('click', this.checkStatus);
+		newItem.querySelector('.js-todo-item__delete').addEventListener('click', this.deleteItem);
+		return newItem;
 	}
 
-	this.checkStatus = function () {
-		const checkboxes = document.querySelectorAll('.js-todo-item__checkbox');
-
-		checkboxes.forEach(function (checkbox) {
-			checkbox.addEventListener('change', function () {
-				if (this.checked) {
-					checkbox.closest('.todo-item').classList.add('todo-item--checked');
-				} else {
-					checkbox.closest('.todo-item').classList.remove('todo-item--checked');
-				}
-			});
-		});
+	this.checkStatus = function (event) {
+		if (event.target.checked) {
+			event.target.closest('.todo-item').querySelector('.todo-item__description').classList.add('todo-item--checked');
+		} else {
+			event.target.closest('.todo-item').querySelector('.todo-item__description').classList.remove('todo-item--checked');
+		}
 	}
 
-	this.deleteItem = function () {
-		const deleteBtn = document.querySelectorAll('.js-todo-item__delete');
-
-		deleteBtn.forEach(function (button) {
-			button.addEventListener('click', function () {
-				button.closest('.todo-item').remove();
-			});
-		})
+	this.deleteItem = function (event) {
+		event.target.closest('.todo-item').remove();
 	}
 }
 
 const task = new Tasks('.js-form__input', '.js-todos-wrapper');
 
 document.querySelector('.js-form').addEventListener('submit', task.addItem);
-
