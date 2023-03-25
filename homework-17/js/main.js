03897d74c126a2d811dd096f4912c4431cecf783
Pavlo Papanov <papan41k@gmail.com>
2023-03-25 19:24:09 +0200

    Finished homework #17

---
+++ homework-17/js/main.js
@@ -0,0 +1,74 @@
+function UserTable({form, content, userInfo, addButton}) {
+	this.init = function () {
+		form.addEventListener('submit', (event) => {
+			event.preventDefault();
+			this.addUser(
+				form.elements['name'].value,
+				form.elements['phone'].value,
+				form.elements['age'].value
+			);
+		})
+		addButton.addEventListener('click', function () {
+			form.reset();
+			form.classList.add('open');
+		})
+		this.loadUsers();
+	}
+	this.addUser = function (name, phone, age) {
+		const user = {
+			id: Math.floor(Math.random() * 100),
+			name,
+			phone,
+			age,
+		}
+		this.userTemplate(user);
+		form.reset();
+		form.classList.remove('open');
+		const currentUsers = JSON.parse(localStorage.getItem('users')) || [];
+		currentUsers.push(user);
+		localStorage.setItem('users', JSON.stringify(currentUsers));
+		location.reload();
+	}
+	this.loadUsers = function () {
+		const users = JSON.parse(localStorage.getItem('users'));
+		if (users) {
+			users.forEach(user => this.userTemplate(user))
+		}
+	}
+	this.userTemplate = function (user) {
+		content.insertAdjacentHTML('beforeend', (
+			`<tr>` +
+			`<td>${user.id}</td>` +
+			`<td>${user.name}</td>` +
+			`<td>${user.phone}</td>` +
+			`<td>${user.age}</td>` +
+			`<td><button class="btn">View</button><button class="btn">Edit</button><button class="btn js--delete">Delete</button></td>` +
+			`</tr>`
+		))
+	}
+	this.deleteUser = function () {
+		const currentUsers = JSON.parse(localStorage.getItem('users'));
+		const deleteButtons = content.querySelectorAll('.js--delete');
+
+		deleteButtons.forEach((elem, index) => {
+			const userRow = elem.closest('tr');
+
+			elem.addEventListener('click', function () {
+				userRow.remove();
+				currentUsers.splice(index, 1);
+				localStorage.setItem('users', JSON.stringify(currentUsers));
+				location.reload();
+			})
+		});
+	}
+}
+
+let userTable = new UserTable({
+	form: document.querySelector('.js--form'),
+	userInfo: document.querySelector('.js--user'),
+	addButton: document.querySelector('.js--add'),
+	content: document.querySelector('.js--content'),
+});
+
+userTable.init();
+userTable.deleteUser();
