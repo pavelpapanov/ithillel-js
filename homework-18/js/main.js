266b77d5afed0d7a1d35d1f7f74cb1c780dd7b58
Pavlo Papanov <papan41k@gmail.com>
2023-03-26 14:55:02 +0300

    Finished homework-18

--- homework-18/js/main.js
+++ homework-18/js/main.js
@@ -1,1 +1,17 @@
-'use strict';
+$(document).ready(function() {
+	let $_galItem = $('.js--gal_item');
+	let $_modal = $('.js--modal');
+
+	$_galItem.on('click', function() {
+		$_modal.addClass('active');
+		let $_imageUrl = $(this).find('img').attr('src');
+		$('.js--modal__content img').attr('src', $_imageUrl);
+		console.log($_imageUrl);
+	});
+
+	$('.js--modal__close').on('click', function () {
+		if ($_modal.has('active')) {
+			$_modal.removeClass('active');
+		}
+	});
+});
